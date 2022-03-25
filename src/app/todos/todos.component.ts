52b2429a438a29d3f8dccaa63e6cf3fe4351626a
Adeleke Hammed Lekan <lekhad19@gmail.com>
2022-03-24 20:20:48 -0400

    Creating reusable animation

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,6 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, keyframes, state, style, transition, trigger, useAnimation } from '@angular/animations';
+import { bounceOutLeftAnimation, fade, slide, fadeInAnimation } from './../animations';
+// import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,16 +8,46 @@ import { Component } from '@angular/core';
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
   animations: [
-    trigger('fade', [
-      transition('void => *', [
-        // style({ backgroundColor : 'yellow', opacity: 0 }),
-        style({ opacity: 0 }),
-        animate(2000)
-        // animate(2000, style({ backgroundColor: 'white', opacity: 1}))
+    // trigger('fade', [
+    //   state('void', style({ opacity: 0 })), 
+
+    //   transition(':enter, :leave', [ // void <=> *
+    //   // transition('void => *, * => void', [
+    //   // transition('void <=> *', [
+    //     // style({ backgroundColor : 'yellow', opacity: 0 }),
+    //     // style({ opacity: 0 }),
+    //     animate(2000)
+    //     // animate(2000, style({ backgroundColor: 'white', opacity: 1}))
+    //   ]), 
+
+    //   // transition('* => void', [
+    //   //   animate(2000
+    //   //     // , 
+    //   //     // style({ opacity: 0 })
+    //   //   )
+    //   // ])
+    // ])
+
+    // fade
+    // slide
+
+    trigger('todoAnimation', [
+      transition(':enter', [
+        // style({ opacity: 0 }),
+        // animate(2000)      
+        useAnimation(fadeInAnimation, {
+          params: {
+            duration: '500ms'
+          }
+        })  
+      ]),
+      transition(':leave', [
+        style({ backgroundColor: 'red' }),
+        animate(1000),
+        useAnimation(bounceOutLeftAnimation)
       ])
     ])
   ]
-  
 })
 export class TodosComponent {
   items: any[] = [
